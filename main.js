61ad6954996048ed4a9047d6cfbfe5fa8236a683
mustafemohamoud <mustyymo@outlook.com>
2024-03-13 21:48:00 +0000

    main.js begun

---
+++ main.js
@@ -0,0 +1,51 @@
+function GetInfo() {
+  const newName = document.getElementById("cityInput");
+  const cityName = document.getElementById("cityName");
+  cityName.innerHTML = "_ _" + newName.value + "_ _";
+
+  fetch(
+    "https://api.openweathermap.org/data/2.5/forecast?q=" +newName.value+ "&appid=23d4bea70582f4647de25585f3d9baeb"
+  )
+    .then((respone) => respone.json())
+    .then((data) => {
+      for (i = 0; i < 5; i++) {
+        document.getElementById("day" + (i + 1) + "Min").innerHTML =
+          "Min:" + Number(data.list[i].main.temp_min - 288.53).toFixed(1) + "°";
+      }
+      for (i = 0; i < 5; i++) {
+        document.getElementById("day" + (i + 1) + "Max").innerHTML =
+          "Max:" + Number(data.list[i].main.temp_max - 288.53).toFixed(1) + "°";
+      }
+      for (i = 0; i < 5; i++) {
+        document.getElementById("img" + (i + 1)).src =
+          "https://openweathermap.org/img/wn/" +
+          data.list[i].weather[0].icon +
+          ".png";
+      }
+    })
+
+    .catch((err) => alert("Something Went Wrong"));
+}
+
+const d = new Date();
+const weekday = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+function CheckDay(day) {
+  if (day + d.getDay() > 6) {
+    return day + d.getDay() - 7;
+  } else {
+    return day + d.getDay();
+  }
+}
+
+// for ( i = 0; i < 5; i++) {
+//     document.getElementById("day"+(i+1)).innerHTML = weekday[CheckDay(i)];
+// }
